aa1ec095f4b374c65f30cb2a9ee17f1df84324b0
GIACOMOALBERTAZZI <g.albertazzi@protonmail.ch>
2021-04-21 08:12:08 +0200

    useEffect

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+  /* const firstValue = text || "hello world";
+  const secondValue = text && "hello world"; */
+
+  return (
+    <React.Fragment>
+      {/* <h1>{firstValue}</h1>
+      <h2>value: {secondValue}</h2> */}
+      <h1>{text || "John"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <p>there is no error</p>
+        </div>
+      )}
+      {/* {!text && <h1>Hello world</h1>} */}
+    </React.Fragment>
+  );
 };
 
 export default ShortCircuit;
